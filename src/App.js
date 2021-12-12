55d99f1bc9e4871f6e124c73ca4f20a0eb1c807f
AluminumAnalyzer <blueunicon@gmail.com>
2021-12-12 19:33:10 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Details from "./routes/Detail";
+import Navigation from "./components/Navigation";
 import About from "./routes/About";
+
 import "./App.css";
+
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
+        <Navigation />
         <Route path="/movie/:id" element={<Details />} />
         <Route path="/about" element={<About />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" exact={true} element={<Home />} />
       </Routes>
     </Router>
   );
