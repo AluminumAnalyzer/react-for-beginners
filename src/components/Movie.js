9c5c94845bb7d0b33e17ec3e8c50920eda03d584
AluminumAnalyzer <blueunicon@gmail.com>
2021-12-12 21:18:20 +0900

    main

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,19 +5,28 @@ import "../components/Movie.css";
 function Movie({ id, coverImage, title, summary, genres, year }) {
   return (
     <div className="movie">
-      <img src={coverImage} alt={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">
-          <Link to={`/movie/${id}`}>{title}</Link>
-        </h3>
-        <h5 className="movie__year">{year}</h5>
-        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
-        <ul className="movie__genres">
-          {genres.map((g, index) => (
-            <li key={index}>{g}</li>
-          ))}
-        </ul>
-      </div>
+      <Link
+        to={{
+          pathname: "/movie-detail",
+          state: { year, title, summary, coverImage, genres },
+        }}
+      >
+        <img src={coverImage} alt={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">
+            <Link to={`/movie/${id}`}>{title}</Link>
+          </h3>
+          <h5 className="movie__year">{year}</h5>
+          <p>
+            {summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}
+          </p>
+          <ul className="movie__genres">
+            {genres.map((g, index) => (
+              <li key={index}>{g}</li>
+            ))}
+          </ul>
+        </div>
+      </Link>
     </div>
   );
 }
